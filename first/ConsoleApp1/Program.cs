ef842514dc606148d33011d260f48c434626a13f
Abhaya <paikraoabhya@gmail.com>
2023-06-14 14:27:03 +0530

    add calculated

--- first/ConsoleApp1/Program.cs
+++ first/ConsoleApp1/Program.cs
@@ -1,5 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+
+    Console.WriteLine("Enter number");
+int a = int.Parse( Console.ReadLine());
+Console.WriteLine("Enter 2nd number");
+    int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Calculate ");
+    Console.WriteLine(a+ b);
+
 Console.ReadLine();
